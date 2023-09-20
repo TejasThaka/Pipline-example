67ba1657f647cd64f9813b2ec353110736e23cd5
TejasThaka <126861384+TejasThaka@users.noreply.github.com>
2023-09-20 23:45:37 +0530

    UPDATE

--- Test.java
+++ Test.java
@@ -1,7 +1,8 @@
 class Test {
     public static void main(String[] args) {
 
-        System.out.println(" this pipeline project.........!");
-        System.out.println("edit by vs code.....!");
+        System.out.println(" This pipeline project.........!");
+        System.out.println("Edit by vs code.....!");
+        System.out.println("from test claSS");
     }
 }
